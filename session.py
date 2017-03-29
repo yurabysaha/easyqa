57bb3626097bcff8d2bdec82d71c9a3fdc039a25
yurabysaha <yurabysaha@gmail.com>
2017-03-29 16:15:04 +0300

    add _req

--- session.py
+++ session.py
@@ -12,10 +12,10 @@ class Session(object):
         self.token = project_token
         self.login = login
         self.password = password
+        self.s = requests.Session()
         self.sign_in()
 
     def sign_in(self):
-        self.s = requests.Session()
         sign_in_url = self.API_URL + '/api/v1/sign_in'
         userdata = json.dumps({
                                 "user": {
@@ -27,6 +27,43 @@ class Session(object):
         response = json.loads(response.content)
         self.auth_token = response['auth_token']
 
+                # ------------------------- Issues --------------------------
 
-    def create_issue(self):
-        pass
+    def create_issue(self, summary, **kwargs):
+        create_issue_url = self.API_URL + '/api/v1/projects/issues/create'
+        required = {
+            "token": self.token,
+            "auth_token": self.auth_token,
+            "summary": summary
+        }
+        required.update(kwargs)
+        data = json.dumps(required)
+        response = self._req('POST', create_issue_url, data)
+
+    def delete_issue_by_id(self, issue_id):
+        delete_issue_url = self.API_URL + '/api/v1/issues/'+ str(issue_id)
+        data = json.dumps({
+            "token": self.token,
+            "auth_token": self.auth_token
+        })
+        response = self._req('DELETE', delete_issue_url, data)
+
+    def delete_issue_by_id_in_project(self, issue_id):
+        delete_issue_url = self.API_URL + '/api/v1/issues/pid' + str(issue_id)
+        data = json.dumps({
+                "token": self.token,
+                "auth_token": self.auth_token
+            })
+        response = self._req('DELETE', delete_issue_url, data)
+
+    def _req(self, method, url, data):
+        if method == 'POST' or method == 'PUT' or method == 'DELETE':
+            response = self.s.request(method=method, url=url, data=data, headers=self.headers)
+            return response
+
+
+                 # ------------------------- Organization --------------------------
+
+
+
+                # ------------------------- Projects --------------------------
