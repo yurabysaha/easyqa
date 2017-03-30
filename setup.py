8dbcdd0337466e34bbff45fe90f1d33fce22944e
yurabysaha <yurabysaha@gmail.com>
2017-03-30 14:54:22 +0300

    prepare for pip

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'easyqa',
+  packages = ['easyqa'],
+  version = '0.1',
+  description = 'Official EasyQA wrapper',
+  author = 'Yura Bysaha, Oleg Stasiv',
+  author_email = 'yurabysaha@gmail.com, stasiv.oleh@gmail.com',
+  url = 'https://github.com/yurabysaha/easyqa', # use the URL to the github repo
+  download_url = 'https://github.com/yurabysaha/easyqa/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'easyqa', 'automation testing'], # arbitrary keywords
+  classifiers = [],
+)
