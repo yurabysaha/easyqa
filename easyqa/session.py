e2bd56be82af5f542b59335b43cf3a93b7a5a4f8
thinkmobilesqa <oleg.stasiv@thinkmobiles.com>
2017-03-31 11:49:09 +0300

    _req method moved to top, add str() for id, finish Roles

--- easyqa/session.py
+++ easyqa/session.py
@@ -18,26 +18,35 @@ class Session(object):
     def _sign_in(self):
         sign_in_url = self.API_URL + '/api/v1/sign_in'
         userdata = json.dumps({
-                                "user": {
-                                            "email": self.login,
-                                            "password": self.password
-                                        }
-                              })
+            "user": {
+                "email": self.login,
+                "password": self.password
+            }
+        })
         response = self.s.post(sign_in_url, data=userdata, headers=self.headers)
         response = json.loads(response.content)
         self.auth_token = response['auth_token']
 
-                # ------------------------- Issues --------------------------
+    def _req(self, method, url, data=None):
+        if method == 'POST' or method == 'PUT' or method == 'DELETE':
+            return self.s.request(method=method, url=url, data=data, headers=self.headers)
+
+        return self.s.request(method=method, url=url)
+
+        # ------------------------- Issues --------------------------
+
     def get_issues(self):
-        get_issue_url = self.API_URL + '/api/v1/issues?' + 'token='+self.token + '&auth_token='+self.auth_token
+        get_issue_url = self.API_URL + '/api/v1/issues?' + 'token=' + self.token + '&auth_token=' + self.auth_token
         return self._req('GET', get_issue_url)
 
     def get_issue_by_id(self, issue_id):
-        get_issue_url = self.API_URL + '/api/v1/issues/'+ str(issue_id) + '?token=' + self.token + '&auth_token=' + self.auth_token
+        get_issue_url = self.API_URL + '/api/v1/issues/' + str(
+            issue_id) + '?token=' + self.token + '&auth_token=' + self.auth_token
         return self._req('GET', get_issue_url)
 
     def get_issue_by_id_in_project(self, issue_id):
-        get_issue_url = self.API_URL + '/api/v1/issues/pid' + str(issue_id) + '?token=' + self.token + '&auth_token=' + self.auth_token
+        get_issue_url = self.API_URL + '/api/v1/issues/pid' + str(
+            issue_id) + '?token=' + self.token + '&auth_token=' + self.auth_token
         return self._req('GET', get_issue_url)
 
     def create_issue(self, summary, **kwargs):
@@ -52,7 +61,7 @@ class Session(object):
         return self._req('POST', create_issue_url, data)
 
     def update_issue_by_id(self, issue_id, summary, **kwargs):
-        update_issue_url = self.API_URL + '/api/v1/issues/'+ str(issue_id)
+        update_issue_url = self.API_URL + '/api/v1/issues/' + str(issue_id)
         required = {
             "token": self.token,
             "auth_token": self.auth_token,
@@ -62,8 +71,8 @@ class Session(object):
         data = json.dumps(required)
         return self._req('PUT', update_issue_url, data)
 
-    def update_issue_by_id_in_project(self,issue_id, summary, **kwargs):
-        update_issue_url = self.API_URL + '/api/v1/issues/pid'+ str(issue_id)
+    def update_issue_by_id_in_project(self, issue_id, summary, **kwargs):
+        update_issue_url = self.API_URL + '/api/v1/issues/pid' + str(issue_id)
         required = {
             "token": self.token,
             "auth_token": self.auth_token,
@@ -74,7 +83,7 @@ class Session(object):
         return self._req('PUT', update_issue_url, data)
 
     def delete_issue_by_id(self, issue_id):
-        delete_issue_url = self.API_URL + '/api/v1/issues/'+ str(issue_id)
+        delete_issue_url = self.API_URL + '/api/v1/issues/' + str(issue_id)
         data = json.dumps({
             "token": self.token,
             "auth_token": self.auth_token
@@ -84,31 +93,26 @@ class Session(object):
     def delete_issue_by_id_in_project(self, issue_id):
         delete_issue_url = self.API_URL + '/api/v1/issues/pid' + str(issue_id)
         data = json.dumps({
-                "token": self.token,
-                "auth_token": self.auth_token
-            })
+            "token": self.token,
+            "auth_token": self.auth_token
+        })
         return self._req('DELETE', delete_issue_url, data)
 
-    def _req(self, method, url, data=None):
-        if method == 'POST' or method == 'PUT' or method == 'DELETE':
-            return self.s.request(method=method, url=url, data=data, headers=self.headers)
 
-        return self.s.request(method=method, url=url)
+        # ------------------------- Organization --------------------------
 
-
-                 # ------------------------- Organization --------------------------
     def get_organizations(self):
         get_organizations_url = self.API_URL + '/api/v1/organizations' + '?auth_token=' + self.auth_token
         return self._req('GET', get_organizations_url)
 
     def show_organization(self, id):
-        show_organizations_url = self.API_URL + '/api/v1/organizations/' + id + '?auth_token=' + self.auth_token
+        show_organizations_url = self.API_URL + '/api/v1/organizations/' + str(id) + '?auth_token=' + self.auth_token
         return self._req('GET', show_organizations_url)
 
     def create_organization(self, title, description=None):
         create_organization_url = self.API_URL + '/api/v1/organizations/'
         data = json.dumps({
-            "organization":{
+            "organization": {
                 "title": title,
                 "description": description
             },
@@ -117,7 +121,7 @@ class Session(object):
         return self._req('POST', create_organization_url, data)
 
     def update_organization(self, id, title=None, description=None):
-        create_organization_url = self.API_URL + '/api/v1/organizations/' + id
+        create_organization_url = self.API_URL + '/api/v1/organizations/' + str(id)
         data = json.dumps({
             "organization": {
                 "title": title,
@@ -128,7 +132,7 @@ class Session(object):
         return self._req('PUT', create_organization_url, data)
 
     def delete_organization(self, id):
-        delete_organization_url = self.API_URL + '/api/v1/organizations/' + id
+        delete_organization_url = self.API_URL + '/api/v1/organizations/' + str(id)
         data = json.dumps({
             "auth_token": self.auth_token
         })
@@ -136,20 +140,21 @@ class Session(object):
 
 
 
-                # ------------------------- Projects --------------------------
+        # ------------------------- Projects --------------------------
+
     def get_projects(self):
         get_projects_url = self.API_URL + '/api/v1/projects' + '?auth_token=' + self.auth_token
         return self._req('GET', get_projects_url)
 
     def show_project(self, id):
-        show_project_url = self.API_URL + '/api/v1/projects/' + id + '?auth_token=' + self.auth_token
+        show_project_url = self.API_URL + '/api/v1/projects/' + str(id) + '?auth_token=' + self.auth_token
         return self._req('GET', show_project_url)
 
     def create_project(self, org_id, title):
         create_project_url = self.API_URL + '/api/v1/projects/'
         data = json.dumps({
-            "organization_id": org_id,
-            "project":{
+            "organization_id": str(org_id),
+            "project": {
                 "title": title
             },
             "auth_token": self.auth_token
@@ -157,9 +162,9 @@ class Session(object):
         return self._req('POST', create_project_url, data)
 
     def update_project(self, id, title=None):
-        update_project_url = self.API_URL + '/api/v1/projects/' + id
+        update_project_url = self.API_URL + '/api/v1/projects/' + str(id)
         data = json.dumps({
-            "project":{
+            "project": {
                 "title": title
             },
             "auth_token": self.auth_token
@@ -167,8 +172,64 @@ class Session(object):
         return self._req('PUT', update_project_url, data)
 
     def delete_project(self, id):
-        delete_project_url = self.API_URL + '/api/v1/projects/' + id
+        delete_project_url = self.API_URL + '/api/v1/projects/' + str(id)
         data = json.dumps({
             "auth_token": self.auth_token
         })
         return self._req('DELETE', delete_project_url, data)
+
+
+        # ------------------------- Roles --------------------------
+
+    def get_roles(self, organization_id):
+        get_roles_url = self.API_URL + "/api/v1/organizations/" + str(organization_id) + \
+                        "/roles?auth_token=" + self.auth_token
+        return self._req('GET', get_roles_url)
+
+    def show_role(self, id):
+        show_role_url = self.API_URL + "/api/v1/roles/" + str(id) + "?auth_token=" + self.auth_token
+        return self._req('GET', show_role_url)
+
+    def create_organization_role(self, organization_id, user_id, role='user'):
+        # Role in organization. Must be "user" or "admin"
+        create_organization_role_url = self.API_URL + '/api/v1/organizations/' + str(organization_id) + '/roles'
+        data = json.dumps(
+            {
+                "role": role,
+                "user_id": str(user_id),
+                "auth_token": self.auth_token
+            }
+        )
+        return self._req('POST', create_organization_role_url, data)
+
+    def create_project_role(self, organization_id, user_id, role='tester'):
+        # Role in project. Must be "developer", "tester", "viewer" or "project_manager"
+        create_project_role_url = self.API_URL + '/api/v1/organizations/' + str(organization_id) + '/roles'
+        data = json.dumps(
+            {
+                "role": role,
+                "user_id": str(user_id),
+                "token": self.token,
+                "auth_token": self.auth_token
+            }
+        )
+        return self._req('POST', create_project_role_url, data)
+
+    def update_project_role(self, id, user_id, role='tester'):
+        # Role in project. Must be "developer", "tester", "viewer" or "project_manager"
+        update_project_role_url = self.API_URL + '/api/v1/roles/' + str(id)
+        data = json.dumps(
+            {
+                "token": self.token,
+                "role": role,
+                "auth_token": self.auth_token
+            }
+        )
+        return self._req('PUT', update_project_role_url, data)
+
+    def delete_role(self, id):
+        delete_role_url = self.API_URL + '/api/v1/roles/' + str(id)
+        data = json.dumps({
+            "auth_token": self.auth_token
+        })
+        return self._req('DELETE', delete_role_url, data)
