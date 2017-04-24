ff840087b55e98386880e1929b4dd09082bf9a53
yurabysaha <yurabysaha@gmail.com>
2017-04-24 12:11:18 +0300

    version 0.1 setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
   description = 'Official EasyQA wrapper',
   author = 'Yura Bysaha, Oleg Stasiv',
   author_email = 'yurabysaha@gmail.com, stasiv.oleh@gmail.com',
-  url = 'https://github.com/yurabysaha/easyqa', # use the URL to the github repo
+  url = 'https://github.com/thinkmobiles/easyqa_library_python', # use the URL to the github repo
   download_url = 'https://github.com/thinkmobiles/easyqa_library_python/archive/0.1.tar.gz', # I'll explain this in a second
   keywords = ['testing', 'easyqa', 'automation testing'], # arbitrary keywords
   classifiers = [],
